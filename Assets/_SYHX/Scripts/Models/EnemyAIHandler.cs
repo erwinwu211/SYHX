6ea1c49262ab2c2560cff425c76ef5d702954234
lee <q2298478324@gmail.com>
2019-05-05 23:58:19 +0900

    敌方ai初步

---
+++ Assets/_SYHX/Scripts/Models/EnemyAIHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Enemy))]
+[RequireComponent(typeof(PlayMakerFSM))]
+public class EnemyAIHandler : MonoBehaviour
+{
+    public Enemy enemy;
+    public PlayMakerFSM fsm;
+    void Awake()
+    {
+        enemy = this.GetComponent<Enemy>();
+        fsm = this.GetComponent<PlayMakerFSM>();
+    }
+}
---
+++ Assets/_SYHX/Scripts/Models/InvokeAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HutongGames.PlayMaker;
+
+[ActionCategory("SYHX")]
+[Tooltip("Action Invoker（激活敌方行动）")]
+public class InvokeAction : FsmStateAction
+{
+    private EnemyAIHandler handler;
+    public override void Awake()
+    {
+        handler = Fsm.GameObject.GetComponent<EnemyAIHandler>();
+    }
+    public override void OnEnter()
+    {
+
+    }
+}
