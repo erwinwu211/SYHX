9126d2c246eb2cb78bf37bcd2ae9d09bd397898f
cardkun <cardkun1917@gmail.com>
2019-05-11 12:23:19 +0900

    装备类初步、修改了地宫角色信息类

--- Assets/_SYHX/Scripts/CharacterInDungeon.cs
+++ Assets/_SYHX/Scripts/CharacterInDungeon.cs
@@ -5,40 +5,73 @@ using System.Linq;
 
 public class CharacterInDungeon : MonoBehaviour
 {
-    protected int hp_max;
+    protected int hp_max_from_job;
+    protected int hp_max_from_dungeon;
     protected int hp_currect;
-    protected int attack;
+    protected int attack_from_job;
+    protected int attack_from_dungeon;
     protected int energy_max;
     protected int draw_count;
     protected List<CardSource> job_deck;
     protected List<CardSource> equip_deck;
+    protected List<EquipmentSource> equipments;
 
-    public int GetHpMax() => hp_max;
+    public int GetHpMax()
+    {
+        int hp_max = hp_max_from_job + hp_max_from_dungeon;
+        foreach (EquipmentSource equipment in equipments)
+        {
+            hp_max += equipment.Hp;
+        }
+        return hp_max;
+    }
     public int GetHpCurrent() => hp_currect;
-    public int GetAttack() => attack;
+    public int GetAttack()
+    {
+        int attack = attack_from_job + attack_from_dungeon;
+        foreach (EquipmentSource equipment in equipments)
+        {
+            attack += equipment.Attack;
+        }
+        return attack;
+    }
     public int GetEnergyMax() => energy_max;
     public int GetDrawCount() => draw_count;
-    public List<CardSource> GetDeck() => job_deck.Concat(equip_deck).ToList();
+    public List<CardSource> GetDeck()
+    {
+        foreach (EquipmentSource equipment in equipments)
+        {
+            foreach (CardSource card in equipment.Cards)
+            {
+                equip_deck.Add(card);
+            }
+        }
+        return job_deck.Concat(equip_deck).ToList();
+    }
 
     public void IncreaseHpMax(int count)
     {
         if (count < 0) return;
-        hp_max += count;
+        hp_max_from_dungeon += count;
     }
     public void DecreaseHpMax(int count)
     {
         if (count < 0) return;
-        hp_max -= count;
-        if (hp_currect < hp_max)
+        hp_max_from_dungeon -= count;
+        if (hp_currect < GetHpMax())
         {
-            hp_currect = hp_max;
+            hp_currect = GetHpMax();
+        }
+        if (GetHpMax() <= 0)
+        {
+            //TODO:SendEvent("DungeonEnd");
         }
     }
     public void IncreaseHpCurrect(int count)
     {
         if (count < 0) return;
         hp_currect += count;
-        if (hp_currect > hp_max) hp_currect = hp_max;
+        if (hp_currect > GetHpMax()) hp_currect = GetHpMax();
     }
     public void DecreaseHpCurrect(int count)
     {
@@ -59,5 +92,16 @@ public class CharacterInDungeon : MonoBehaviour
     public void JoinCard(CardSource card)
     {
         job_deck.Add(card);
+    }
+    public void Equip(EquipmentSource equipment)
+    {
+        equipments.Add(equipment);
+    }
+    public void Unequip(EquipmentSource equipment)
+    {
+        if (equipments.Contains(equipment))
+        {
+            equipments.Remove(equipment);
+        }
     }
 }
