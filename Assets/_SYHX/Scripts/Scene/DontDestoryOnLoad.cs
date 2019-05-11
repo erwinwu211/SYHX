8ca1845f1b0c836698ccf25f5c24a222b56b0127
cardkun <cardkun1917@gmail.com>
2019-05-11 09:07:04 +0900

    场景切换状态机

---
+++ Assets/_SYHX/Scripts/Scene/DontDestoryOnLoad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestoryOnLoad : MonoBehaviour
+{
+    public bool Active;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        this.gameObject.SetActive(Active);
+    }
+}
