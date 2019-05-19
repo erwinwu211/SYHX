28347b7a118d15f9bf8f81d1745d728cd0f2e43f
cardkun <cardkun1917@gmail.com>
2019-05-20 08:30:39 +0900

    完善角色界面

--- Assets/_SYHX/Scripts/UI/CharacterUI.cs
+++ Assets/_SYHX/Scripts/UI/CharacterUI.cs
@@ -1,11 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CharacterUI : MonoBehaviour
 {
+    private GameObject TalentBtnTip;
+    private CharacterContent selectedCharacter;
+    private Transform Info;
+    private TextMeshProUGUI NameText;
+    private TextMeshProUGUI AttackCount;
+    private TextMeshProUGUI DefendCount;
+    private TextMeshProUGUI PowerCount;
+    private TextMeshProUGUI DrawCount;
+    private TextMeshProUGUI DeckCount;
+
+    private CharacterContent FukasakiKotone;
+
+    void Start()
+    {
+        //读取界面
+        TalentBtnTip = GameObject.Find("TalentBtn/Tip");
+        Info = GameObject.Find("Info").transform;
+        NameText = GameObject.Find("Name").GetComponent<TextMeshProUGUI>();
+        AttackCount = Info.Find("Attack/Count").GetComponent<TextMeshProUGUI>();
+        DefendCount = Info.Find("Defend/Count").GetComponent<TextMeshProUGUI>();
+        PowerCount = Info.Find("Power/Count").GetComponent<TextMeshProUGUI>();
+        DrawCount = Info.Find("DrawCount/Count").GetComponent<TextMeshProUGUI>();
+        DeckCount = Info.Find("DeckCount/Count").GetComponent<TextMeshProUGUI>();
+
+        //创建好人物信息
+        FukasakiKotone = new FukasakiKotone().GenerateCharacter();
+
+        //进行初始化设定
+        selectedCharacter = FukasakiKotone;
+        TalentBtnTip.SetActive(false);
+        RefreshSelectedCharacterInfo();
+    }
+
+    /// <summary>
+    /// 根据选中的人物刷新显示数据
+    /// </summary>
+    private void RefreshSelectedCharacterInfo()
+    {
+        NameText.text = selectedCharacter.Name;
+        AttackCount.text = selectedCharacter.Attack+"";
+        DefendCount.text = selectedCharacter.Defend + "";
+        PowerCount.text = selectedCharacter.Energy + "";
+        DrawCount.text = selectedCharacter.DrawCount + "";
+        DeckCount.text = selectedCharacter.Cards.Count + "";
+    }
+
+    /// <summary>
+    /// 按返回按钮回到主界面
+    /// </summary>
     public void OnReturnBtnClick()
     {
         SceneStateManager.Ins.SetSceneStatus(new MainState(SceneStateManager.Ins));
+    }
+
+    public void OnToggleChanged(int i)
+    {
+        switch (i)
+        {
+            case 1:
+                selectedCharacter = FukasakiKotone;
+                return;
+            case 2:
+                return;
+            case 3:
+                return;
+            default:
+                return;
+        }
     }
 }
