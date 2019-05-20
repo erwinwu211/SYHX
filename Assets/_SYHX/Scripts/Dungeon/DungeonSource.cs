0dfe5e39f3275f520f407393b7b015f2e8de8ac8
cardkun <cardkun1917@gmail.com>
2019-05-20 10:53:33 +0900

    地宫选择界面的处理

---
+++ Assets/_SYHX/Scripts/Dungeon/DungeonSource.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonSource : ScriptableObject
+{
+    public string Name { get; protected set; }
+    public string Desc { get; protected set; }
+    public Vector3 Pos { get; protected set; }
+    public string Icon { get; protected set; }
+    //public List<RoomSource> rooms { get; protected set; }
+
+    public virtual DungeonContent GenerateDungeon()
+    {
+        var dc = new DungeonContent();
+        dc.SetOwner(this);
+        return dc;
+    }
+
+}
--- Assets/_SYHX/Scripts/UI/MapUI.cs
+++ Assets/_SYHX/Scripts/UI/MapUI.cs
@@ -1,9 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapUI : MonoBehaviour
 {
+    public GameObject DungeonGO;
+    public GameObject DungeonInfoGO;
+    public GameObject DungeonParent;
+    private DungeonContent d01;
+    private DungeonContent d02;
+    private DungeonContent d03;
+    private DungeonContent d04;
+    private List<DungeonContent> dungeons;
+
+    private void Start()
+    {
+        d01 = new Dungeon01().GenerateDungeon();
+        d02 = new Dungeon01().GenerateDungeon();
+        d03 = new Dungeon01().GenerateDungeon();
+        d04 = new Dungeon01().GenerateDungeon();
+        dungeons = new List<DungeonContent>();
+        dungeons.Add(d01);
+        dungeons.Add(d02);
+        dungeons.Add(d03);
+        dungeons.Add(d04);
+        foreach (DungeonContent dc in dungeons)
+        {
+            GameObject go = GameObject.Instantiate(DungeonGO, dc.Pos, Quaternion.identity, DungeonParent.transform);
+            go.transform.localPosition = dc.Pos;
+        }
+
+    }
+
     public void OnReturnBtnClick()
     {
         SceneStateManager.Ins.SetSceneStatus(new MainState(SceneStateManager.Ins));
