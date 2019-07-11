8471003086c6144e34125af96c2e18b040f03202
cardkun <cardkun1917@gmail.com>
2019-07-11 16:30:20 +0900

    添加了主界面时间显示与按钮效果

---
+++ Assets/_SYHX/Scripts/UI/MainUI/ButtonEffect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonEffect : MonoBehaviour,
+    IPointerEnterHandler,IPointerExitHandler,IPointerClickHandler
+{
+    public GameObject TextPic;
+    public GameObject BgPic;
+    public Color Normal;
+    public Color HighLight;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        TextPic.GetComponent<Image>().color = HighLight;
+        BgPic.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        TextPic.GetComponent<Image>().color = Normal;
+        BgPic.SetActive(false);
+    }
+}
