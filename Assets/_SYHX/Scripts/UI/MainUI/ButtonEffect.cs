8d4b2d52ae3b89fc56b49ec27a999b9f23a71a45
cardkun <cardkun1917@gmail.com>
2019-07-12 09:36:54 +0900

    实现了按钮背景填充效果

--- Assets/_SYHX/Scripts/UI/MainUI/ButtonEffect.cs
+++ Assets/_SYHX/Scripts/UI/MainUI/ButtonEffect.cs
@@ -11,6 +11,7 @@ public class ButtonEffect : MonoBehaviour,
     public GameObject BgPic;
     public Color Normal;
     public Color HighLight;
+    public float FillSpeed = 0.18f;
 
     public void OnPointerClick(PointerEventData eventData)
     {
@@ -20,11 +21,23 @@ public class ButtonEffect : MonoBehaviour,
     {
         TextPic.GetComponent<Image>().color = HighLight;
         BgPic.SetActive(true);
+        BgPic.GetComponent<Image>().fillAmount = 0;
+        StartCoroutine(FillBg());
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         TextPic.GetComponent<Image>().color = Normal;
         BgPic.SetActive(false);
+    }
+
+    IEnumerator FillBg()
+    {
+        Image image = BgPic.GetComponent<Image>();
+        for(; image.fillAmount<1;)
+        {
+            image.fillAmount += FillSpeed;
+            yield return new WaitForSeconds(0.02f);
+        }
     }
 }
