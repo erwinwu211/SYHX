dce2de9d213d8457cb14af54a6aa8cd629ba51a1
lee <q2298478324@gmail.com>
2019-07-31 17:01:14 +0900

    enemygroupmanager改单例

--- Assets/_SYHX/Scripts/Models/EnemyGroupManager.cs
+++ Assets/_SYHX/Scripts/Models/EnemyGroupManager.cs
@@ -2,16 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyGroupManager : MonoBehaviour
+public class EnemyGroupManager : SingletonMonoBehaviour<EnemyGroupManager>
 {
-    public static EnemyGroupManager Ins { get; private set; }
+
     public GameObject EnemyParent;
     [SerializeField] public List<EnemyGroup> enemyGroup;
-    void Awake()
-    {
-        if (Ins == null)
-        {
-            Ins = this;
-        }
-    }
+    protected override void UnityAwake() { }
 }
